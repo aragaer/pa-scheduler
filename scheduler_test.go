9d3d4bae98dc80e1dcf3314095d0c10b81f38f65
aragaer <aragaer@gmail.com>
2018-01-07 00:56:26 +0300

    Rewrite tests for readability

--- scheduler_test.go
+++ scheduler_test.go
@@ -1,194 +1,76 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
-func MakeEvent(delay int64, repeat int64, name string) *Event {
-	return &Event{
-		Delay:  delay,
-		Repeat: repeat,
-		Name:   name,
-		What:   []byte(`{"event": "tick", "to": "brain1"}`),
-	}
+type event struct {
+	delay, repeat int64
+	name          string
 }
 
-func TestScheduler_ImmediateOnce(t *testing.T) {
-	scheduler := NewScheduler()
-	name := "tick once to brain1"
-
-	scheduler.Queue(MakeEvent(0, 0, name))
-
-	event := scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name)
-	} else if event.Name != name {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
+func (e event) mk() *Event {
+	return &Event{e.delay, e.repeat, e.name, []byte(`{"event": "tick"}`)}
 }
 
-func TestScheduler_DelayedOnce(t *testing.T) {
-	scheduler := NewScheduler()
-	name := "tick once to brain1"
+type Events []event
+type Expected []string
 
-	scheduler.Queue(MakeEvent(1, 0, name))
-
-	event := scheduler.GetTriggeredEvent()
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-
-	scheduler.Tick(1)
-
-	event = scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name)
-	} else if event.Name != name {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
+type tc struct {
+	events   Events
+	expected Expected
 }
 
-func TestScheduler_Repeating(t *testing.T) {
-	scheduler := NewScheduler()
-	name := "tick to brain1"
-
-	scheduler.Queue(MakeEvent(0, 1, name))
-
-	event := scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name)
-	} else if event.Name != name {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-
-	scheduler.Tick(1)
-
-	event = scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not repeated", name)
-	} else if event.Name != name {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
+var testCases = map[string]tc{
+	"one immediate": {
+		Events{{0, 0, "tick"}},
+		Expected{"tick", "", ""}},
+	"one delayed": {
+		Events{{1, 0, "tick"}},
+		Expected{"", "tick", ""}},
+	"one repeating": {
+		Events{{0, 1, "tick"}},
+		Expected{"tick", "tick", "tick"}},
+	"two immediate": {
+		Events{{0, 0, "tick"}, {0, 0, "tock"}},
+		Expected{"tick tock", "", ""}},
+	"two alternating": {
+		Events{{0, 2, "tick"}, {1, 2, "tock"}},
+		Expected{"tick", "tock", "tick", "tock"}},
+	"two different freq": {
+		Events{{0, 2, "tick"}, {0, 3, "tock"}},
+		Expected{"tick tock", "", "tick", "tock"}},
+	"insert before": {
+		Events{{1, 0, "tock"}, {0, 0, "tick"}},
+		Expected{"tick", "tock", ""}},
 }
 
-func TestScheduler_TwoImmediateOnce(t *testing.T) {
-	scheduler := NewScheduler()
-	name1 := "tick1"
-	name2 := "tick2"
+func TestScheduler(t *testing.T) {
+	for name, tc := range testCases {
+		scheduler := NewScheduler()
+		for _, e := range tc.events {
+			scheduler.Queue(e.mk())
+		}
 
-	scheduler.Queue(MakeEvent(0, 0, name1))
-	scheduler.Queue(MakeEvent(0, 0, name2))
+		for tick, eventsForTick := range tc.expected {
+			for _, expected := range strings.Fields(eventsForTick) {
+				event := scheduler.GetTriggeredEvent()
+				if event == nil {
+					t.Errorf("Test case \"%s\" failed:", name)
+					t.Fatalf("\"%s\" event is not triggered on tick %d", expected, tick)
+				} else if event.Name != expected {
+					t.Errorf("Test case \"%s\" failed:", name)
+					t.Fatalf("Expected \"%s\", got \"%s\"", expected, event.Name)
+				}
+			}
+			event := scheduler.GetTriggeredEvent()
+			if event != nil {
+				t.Errorf("Test case \"%s\" failed:", name)
+				t.Fatalf("\"%s\" event has ticked on tick %d", event.Name, tick)
+			}
 
-	event := scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name1)
-	} else if event.Name != name1 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name1, event.Name)
-	}
-
-	event = scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name2)
-	} else if event.Name != name2 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name2, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-}
-
-func TestScheduler_TwoAlternateRepeating(t *testing.T) {
-	scheduler := NewScheduler()
-	name1 := "tick1"
-	name2 := "tick2"
-
-	scheduler.Queue(MakeEvent(0, 2, name1))
-	scheduler.Queue(MakeEvent(1, 2, name2))
-
-	event := scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name1)
-	} else if event.Name != name1 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name1, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-
-	scheduler.Tick(1)
-
-	event = scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name2)
-	} else if event.Name != name2 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name2, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-
-	scheduler.Tick(1)
-
-	event = scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name1)
-	} else if event.Name != name1 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name1, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-}
-
-func TestScheduler_ScheduleBefore(t *testing.T) {
-	scheduler := NewScheduler()
-	name1 := "tick1"
-	name2 := "tick2"
-
-	scheduler.Queue(MakeEvent(1, 0, name2))
-	scheduler.Queue(MakeEvent(0, 0, name1))
-
-	event := scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name1)
-	} else if event.Name != name1 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name1, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
-	}
-
-	scheduler.Tick(1)
-
-	event = scheduler.GetTriggeredEvent()
-	if event == nil {
-		t.Fatalf("\"%s\" event is not triggered", name2)
-	} else if event.Name != name2 {
-		t.Fatalf("Expected \"%s\", got \"%s\"", name2, event.Name)
-	}
-
-	if scheduler.GetTriggeredEvent() != nil {
-		t.Fatalf("\"%s\" event has ticked", event.Name)
+			scheduler.Tick(1)
+		}
 	}
 }
