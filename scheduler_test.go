99be5d5f7ff02c30d7e2d0b1517e755fb78b4ca3
aragaer <aragaer@gmail.com>
2018-01-07 20:40:19 +0300

    Refactor scheduler test

--- scheduler_test.go
+++ scheduler_test.go
@@ -18,65 +18,79 @@ type Events []event
 type Expected []string
 
 type tc struct {
+	name     string
 	events   Events
 	expected Expected
 }
 
-var testCases = map[string]tc{
-	"nothing": {
+var testCases = []tc{
+	{"nothing",
 		Events{},
 		Expected{"", "", ""}},
-	"one immediate": {
+	{"one immediate",
 		Events{{0, 0, "tick"}},
 		Expected{"tick", "", ""}},
-	"one delayed": {
+	{"one delayed",
 		Events{{1, 0, "tick"}},
 		Expected{"", "tick", ""}},
-	"one repeating": {
+	{"one repeating",
 		Events{{0, 1, "tick"}},
 		Expected{"tick", "tick", "tick"}},
-	"two immediate": {
+	{"two immediate",
 		Events{{0, 0, "tick"}, {0, 0, "tock"}},
 		Expected{"tick tock", "", ""}},
-	"two alternating": {
+	{"two alternating",
 		Events{{0, 2, "tick"}, {1, 2, "tock"}},
 		Expected{"tick", "tock", "tick", "tock"}},
-	"two different freq": {
+	{"two different freq",
 		Events{{0, 2, "tick"}, {0, 3, "tock"}},
 		Expected{"tick tock", "", "tick", "tock", "tick", "", "tick tock"}},
-	"insert before": {
+	{"insert before",
 		Events{{1, 0, "tock"}, {0, 0, "tick"}},
 		Expected{"tick", "tock", ""}},
 }
 
-func TestScheduler(t *testing.T) {
-	for name, tc := range testCases {
-		scheduler := NewScheduler()
-		for _, e := range tc.events {
-			scheduler.Queue(e.mk())
+func (tc *tc) SetUpQueue() *Scheduler {
+	scheduler := NewScheduler()
+	for _, e := range tc.events {
+		scheduler.Queue(e.mk())
+	}
+	return scheduler
+}
+
+func (tc *tc) CheckEvents(scheduler *Scheduler, tick int, t *testing.T) {
+	actual := make(map[string]bool)
+	for event := range scheduler.TriggeredEvents() {
+		actual[event.Name] = true
+	}
+
+	expected := make(map[string]bool)
+	for _, e := range strings.Fields(tc.expected[tick]) {
+		expected[e] = true
+		if !actual[e] {
+			t.Errorf("Test case \"%s\" failed on tick %d", tc.name, tick)
+			t.Errorf("event \"%s\" is expected but didn't happen", e)
 		}
+	}
 
-		for tick, eventsForTick := range tc.expected {
-			actual := make(map[string]bool)
-			for event := range scheduler.TriggeredEvents() {
-				actual[event.Name] = true
-			}
+	for e := range actual {
+		if !expected[e] {
+			t.Errorf("Test case \"%s\" failed on tick %d", tc.name, tick)
+			t.Errorf("event \"%s\" happened but is not expected", e)
+		}
+	}
+}
 
-			expected := make(map[string]bool)
-			for _, e := range strings.Fields(eventsForTick) {
-				expected[e] = true
-				if !actual[e] {
-					t.Errorf("Test case \"%s\" failed on tick %d", name, tick)
-					t.Errorf("event \"%s\" is expected but didn't happen", e)
-				}
-			}
+func TestScheduler(t *testing.T) {
+	for _, tc := range testCases {
+		scheduler := tc.SetUpQueue()
 
-			for e := range actual {
-				if !expected[e] {
-					t.Errorf("Test case \"%s\" failed on tick %d", name, tick)
-					t.Errorf("event \"%s\" happened but is not expected", e)
-				}
-			}
+		for tick := range tc.expected {
+			tc.CheckEvents(scheduler, tick, t)
+			scheduler.Tick(1)
+		}
+	}
+}
 
 			scheduler.Tick(1)
 		}
